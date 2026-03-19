352381f33b80ddd6a6614cfca2eafe873bd41a61
Shadow prime <gning8044@gmail.com>
2026-03-19 23:14:35 +0000

    Delete config.js

--- config.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-    // TES INFORMATIONS (change ce qui est entre "")
-    botName: "𝕯𝖊𝖛 𝕾𝖍𝖆𝖉𝖔𝖜 𝕿𝖊𝖈𝖍",
-    ownerNumber: "221766297256", // Mets TON numéro ICI !
-    prefix: ".",
-    version: "3.0.0"
-};
