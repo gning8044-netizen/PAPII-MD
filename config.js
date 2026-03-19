3c843b76fb0d1bbcfaa8e5b78008fd571f8b1578
Shadow prime <gning8044@gmail.com>
2026-03-19 18:43:31 +0000

    Update config.js

--- config.js
+++ config.js
@@ -1,20 +1,7 @@
 module.exports = {
     // TES INFORMATIONS (change ce qui est entre "")
     botName: "𝕯𝖊𝖛 𝕾𝖍𝖆𝖉𝖔𝖜 𝕿𝖊𝖈𝖍",
-    ownerNumber: "221769325203", // Mets TON numéro ICI !
-    
-    // TES LIENS (tu changeras plus tard)
-    mainVideos: [
-        "https://files.catbox.moe/video1.mp4",
-        "https://files.catbox.moe/video2.mp4",
-        "https://files.catbox.moe/video3.mp4"
-    ],
-    
-    // Le symbole des commandes
+    ownerNumber: "221766297256", // Mets TON numéro ICI !
     prefix: ".",
-    
-    // Fonction pour choisir une vidéo au hasard
-    randomVideo: (videos) => {
-        return videos[Math.floor(Math.random() * videos.length)];
-    }
+    version: "3.0.0"
 };
