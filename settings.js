be5036265bd25796e74527632b61a5bf58b7ba79
Shadow prime <gning8044@gmail.com>
2026-03-20 00:47:31 +0000

    Create settings.js

---
+++ settings.js
@@ -0,0 +1,37 @@
+const settings = {
+  packname: '🌹𝐃𝐄𝐕 𝐒𝐇𝐀𝐃𝐎𝐖 𝐓𝐄𝐂𝐇–MD•V3🌹',
+  author: 'DEV_SHADOW_TECH',
+  botName: "🌹𝐃𝐄𝐕 𝐒𝐇𝐀𝐃𝐎𝐖 𝐓𝐄𝐂𝐇–MD•V3🌹",
+  botOwner: 'STIVO-TECH',
+  phoneNumber: "221769469119",
+  ownerNumber: '221769469119',
+  giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
+  commandMode: "public",
+  maxStoreMessages: 20,
+  storeWriteInterval: 10000,
+  description: "DEV SHADOW TECH MD — Bot WhatsApp en français, rapide, multi-device, créer par DEV_SHADOW_TECH.",
+  version: "3.0.1",
+  channelLink: "https://whatsapp.com/channel/0029VbAlfhMAInPdvqEmAf2N",
+  updateZipUrl: "https://github.com/STIVE-DEVX/ZENTROX-MD-main.zip",
+  botPreview: "https://i.postimg.cc/Dz3mbLwc/bot-image-f-Of28PVl.jpg",
+
+  // Carte globale
+  adReply: {
+    title: "DEV SHADOW TECH BOT MD",
+    body: "BY DEV SHADOW TECH",
+    thumbnailUrl: 'https://i.postimg.cc/c4KWPt69/chatgpt-image-3-dec-2025-17-48-55-PQCmcx-Vn.jpg',
+    sourceUrl: 'https://whatsapp.com/channel/0029VbAlfhMAInPdvqEmAf2N',
+    mediaType: 1,
+    mediaUrl: 'https://whatsapp.com/channel/0029VbAlfhMAInPdvqEmAf2N',
+    renderLargerThumbnail: false
+  },
+
+  // Newsletter global (optionnel, utilisable dans toutes les commandes)
+  newsletter: {
+    newsletterJid: '120363402057857053@newsletter',
+    newsletterName: '🌹𝐃𝐄𝐕 𝐒𝐇𝐀𝐃𝐎𝐖 𝐓𝐄𝐂𝐇–MD•V3🌹',
+    serverMessageId: -1
+  }
+};
+
+module.exports = settings;
